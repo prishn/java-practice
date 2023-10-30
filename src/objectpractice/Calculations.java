f479ea043b8c17ae6a05430c22193f4885803133
prishn <kakadiyaasha@googlemail.com>
2023-10-30 15:05:28 +0000

    calculations

---
+++ src/objectpractice/Calculations.java
@@ -0,0 +1,9 @@
+package objectpractice;
+
+public class Calculations {
+    public static void main(String[] args) {
+        Calculations a = new Calculations();
+        Calculations b = new Calculations();
+        Calculations c = new Calculations();
+    }
+}
